0f10f873ddfdbadbd63e6b6329bf57a9b558fe9a
VitorInacio <vitorinacio16@gmail.com>
2023-05-03 22:17:39 -0300

    03/05/23

--- admin/cadastros/categorias.php
+++ admin/cadastros/categorias.php
@@ -1,4 +1,13 @@
 <div class="card">
+    <div class="card-header">
+        <h2 class="float-left">Cadastro de categorias</h2>
+        
+        <div class="float-right">
+            <a href="listar/categorias" class="btn btn-success">
+                Listar Categorias
+            </a>
+        </div>
+    </div>
     <div class="card-body">
         <form method="POST" action="salvar/categorias">
             <label for="id">ID da categoria</label>
---
+++ admin/listar/categorias.php
@@ -0,0 +1,42 @@
+<?php
+
+    $sql = "SELECT * FROM categoria";
+    $con = $pdo->query($sql);
+    $dados = $con->fetchAll(PDO::FETCH_OBJ);
+
+?>
+<div class="card">
+    <div class="card-header">
+        <h2 class="float-center">Todas as categorias</h2>
+    </div>
+
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php    
+                foreach($dados as $dado){
+                    ?>
+                    <tr>
+                        <th scope="row"><?=$dado->id?></th>
+                        <td><?=$dado->nome?></td>
+
+                        <td>
+                            <a href="salvar/categorias.php?id=<?=$dado->id?>" class="btn btn-danger">excluir</a>
+                            <a href="salvar/categorias.php?id=<?=$dado->id?>" class="btn btn-warning">alterar</a>
+                        </td>
+                    </tr>
+                    <?php
+                };
+                ?>
+                
+            </tbody>
+        </table>
+    </div>
+</div>
